92a36463f7bde7288f6b8125b9148af1d40e2bd3
cquintana001 <cquintana002@gmail.com>
2023-02-07 18:48:56 +0100

    commit 07022023

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -1,6 +1,11 @@
 #include "PhoneBook.hpp"
 #include <iostream>
+#include <cstdlib>
 
+bool is_digits(const std::string &str)
+{
+    return str.find_first_not_of("0123456789") == std::string::npos;
+}
 PhoneBook::PhoneBook()
 {
     std::cout<<"PhoneBook default constructor called"<<std::endl;
@@ -8,6 +13,7 @@ PhoneBook::PhoneBook()
 }
 PhoneBook::~PhoneBook()
 {
+     std::cout<<this->list[0].getFirstName()<<"destruido"<<std::endl;
     std::cout<<"PhoneBook default destructor called"<<std::endl;
 }
 
@@ -17,21 +23,33 @@ void PhoneBook::add()
       
     std::cout<<"Name"<<std::endl;
     std::string name; 
-    std::cin>>name;
+    std::getline(std::cin,name);
+    std::cout<<std::endl; 
     std::cout<<"Last name"<<std::endl;
     std::string last; 
-    std::cin>>last;
+    std::getline(std::cin,last);
+    std::cout<<std::endl; 
     std::string telephone; 
     std::cout<<"Telephone number"<<std::endl;
-    std::cin>>telephone;    
+    std::getline(std::cin,telephone);
+    std::cout<<std::endl;   
+    while(telephone.length()>9 ||!is_digits(telephone))
+    {
+        std::cout<<"Telephone number not valid please try again:"<<std::endl;
+        std::getline(std::cin,telephone); 
+        std::cout<<std::endl; 
+    }
     std::cout<<"Nickname"<<std::endl;
     std::string nickname; 
-    std::cin>>nickname;
+    std::getline(std::cin,nickname);
+    std::cout<<std::endl; 
     std::cout<<"His darkest secret"<<std::endl;
     std::string secret; 
-    std::cin>>secret;
-    std::cout<<telephone<<std::endl;
-     
+    std::getline(std::cin,secret);
+    std::cout<<std::endl;
+    int tel = std::atoi(telephone.c_str());
+    Contact contact = Contact(name, last, nickname,tel ,secret);
+    this->list[0] = contact;
 }
 void PhoneBook::search(int index)
 {
