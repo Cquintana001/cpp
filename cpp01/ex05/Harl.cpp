56933e242ce2f73ec517452d744bbb5b70c1882f
cquintana001 <cquintana002@gmail.com>
2023-05-13 20:05:56 +0200

    ex02

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,46 @@
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+	std::cout << "A wild Harl has appeared!" << std::endl;
+}
+
+Harl::~Harl(void)
+{
+	std::cout << "Wild Harl flew away!" << std::endl;
+}
+
+void Harl::debug(void)
+{
+	std::cout << "I just realized my burger order is longer than my shopping list." << std::endl;
+}
+
+void Harl::info(void)
+{
+	std::cout << "I'm glad this burger place doesn't charge me for extra napkins." << std::endl;
+}
+
+void Harl::warning(void)
+{
+	std::cout << "I always check my order twice, but if it's wrong again, I'm done with this place." << std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout << "You gave me the wrong burger again? That's it, I'm going to the competition." << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+	void (Harl::*ptr_complain[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string levels[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (levels[i] == level)
+		{
+			(this->*ptr_complain[i])();
+			break;
+		}
+	}
+}
